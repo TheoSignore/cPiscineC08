4b713e31d078fa6e05e3349f98809f568bcb24cc
Theo SIGNORE <tsignore@e2r2p16.42.fr>
2020-07-21 13:47:20 +0200

    normalized

--- ex05/ft_show_tab.c
+++ ex05/ft_show_tab.c
@@ -6,17 +6,17 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 18:33:26 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/20 19:02:41 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/21 13:42:05 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stock_str.h"
 #include "my_lib.h"
 
-void ft_show_tab(struct s_stock_str *par)
+void	ft_show_tab(struct s_stock_str *par)
 {
 	int i;
-	
+
 	i = 0;
 	while (par[i].size != 0)
 	{
